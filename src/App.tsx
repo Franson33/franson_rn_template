184765deffd9f347d75007db23dcbd7185b1c554
Franson33 <antonchikfr@gmail.com>
2023-12-30 20:45:22 +0200

    added react-query, added query provider and client to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import React, {JSX} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import {MainScreen} from './screens';
 import {RootStackParams, routeNames} from './navigator/routs';
 
 const RootStack = createNativeStackNavigator<RootStackParams>();
 
+const queryClient = new QueryClient();
+
 export const App = (): JSX.Element => {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <RootStack.Navigator initialRouteName={routeNames.HOME}>
-          <RootStack.Screen name={routeNames.HOME} component={MainScreen} />
-        </RootStack.Navigator>
-      </SafeAreaView>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <RootStack.Navigator initialRouteName={routeNames.HOME}>
+            <RootStack.Screen name={routeNames.HOME} component={MainScreen} />
+          </RootStack.Navigator>
+        </SafeAreaView>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
